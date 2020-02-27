552ec1912208b24bd8f1013b5de7ee9a51876a9e
chenqi1233 <qi.chen@dfrobot.com>
2020-02-27 14:53:54 +0800

    修改config文件

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -1,13 +1,10 @@
-
 //% color="#FF8C00" iconWidth=50 iconHeight=40
 namespace SHT20_I2C_Water{
     //% block="SHT20_I2C_Water init " blockType="command" 
     export function SHT20_I2C_WaterInit(parameter: any, block: any) {
         Generator.addInclude("SHT20_I2C_WaterInit11", "#include <Wire.h>");
         Generator.addInclude("SHT20_I2C_WaterInit22", "#include <DFRobot_SHT20.h>");
-
         Generator.addObject(`SHT20_I2C_WaterDFRobot_SHT20`,  "DFRobot_SHT20",`sht20;`);
-
         Generator.addSetup("SHT20_I2C_Water1","Serial.begin(9600);");
         Generator.addSetup("SHT20_I2C_Water2","sht20.initSHT20();");
         Generator.addSetup("SHT20_I2C_Water3", "delay(100);");
